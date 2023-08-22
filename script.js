5929be6dcff166adb3f59c821cb9f1170786bca7
EshterWuatson <1Esther.Wu@gmail.com>
2023-08-21 21:42:10 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,129 @@
+*/ Write password to the #password input
+var generateBtn = document.querySelector("#generate");
+var add = function (num1, num2) ();
+
+var specialCharactersforinput.[]
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+
+if (passwordPassword)
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+tothefunctionwritingthepassword.addEventListener
+
+specificcharactersforinput.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function writePasswrod() {
+  console.log("writePassword function");
+}
+
+var passwordLength = prompt ()
+  How many characters would you like your password to be?
+);
+if (passwordLength < 10 || passwordLength > 128)
+{
+
+  SpeechRecognitionAlternativeTENmultiple()[]
+}
+
+var useSpecialCharacters = confirm()
+  "Must to use both numeric characters in your password"
+var openspacesincreasingfor404(Not FoundNotFound)
+allowedChars > 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
